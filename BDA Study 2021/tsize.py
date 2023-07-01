8dfa1b4fa70c7649c12525a5d179c91052a42f55
Hanna Park <qkrgkssk8087@gmail.com>
2023-07-01 22:58:10 +0900

    UV BDA class study commit

---
+++ BDA Study 2021/tsize.py
@@ -0,0 +1,10 @@
+size = 'M'
+
+if size == 'S':
+    length, width = 68.0, 52.0
+elif size == 'M':
+    length, width = 70.0, 54.5
+else :
+    length, width = 72.0, 57.0
+    
+print('길이 = ',length, '너비 = ',width)
